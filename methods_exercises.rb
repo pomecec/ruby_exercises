a3c6a45ba1804bbe12b2ddab2f96ea6c55c0a75a
lipeng <pomecec@gmail.com>
2015-06-01 17:42:52 +0800

    methods exercises

---
+++ methods_exercises.rb
@@ -0,0 +1,14 @@
+# 1. Write a program that contains a method called greeting that takes a name as its parameter. 
+# It then prints a greeting message with the name included in it.
+def greeting(name) 
+    "Hello, #{name}. How are you doing?"
+end
+
+puts greeting("Li Peng")
+
+# 3. Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+def multiply(a, b)
+    a * b
+end  
+
+puts multiply(10, 100)
